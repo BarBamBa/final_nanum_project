297c4bdef2e7e1a70228b5d487f8fbcaa57893c7
BarBamBa <rushknight75@gmail.com>
2023-08-04 13:21:42 +0900

    react 페이지 구성

--- frontend/reactjs/src/App.jsx
+++ frontend/reactjs/src/App.jsx
@@ -1,18 +1,25 @@
-import './App.css';
 import {Route, Routes} from 'react-router-dom';
+import About from './pages/about/About';
+import Board from './pages/board/Board';
+import Login from './pages/login/Login';
+import Manager from './pages/manager/Manager';
+import MyPage from './pages/myPage/MyPage';
+import Qna from './pages/qna/Qna';
+import SignUp from './pages/signUp/SignUp';
+import Volunteer from './pages/volunteer/Volunteer';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/about" element="" /> 
-        <Route path="/volunteer/*" element="" />
-        <Route path="/board/*" element="" />
-        <Route path="/mypage/*" element="" />
-        <Route path="/qna/*" element="" />
-        <Route path="/login/*" element="" />
-        <Route path="/signup/*" element="" />
-        <Route path="/manager/*" element="" />
+        <Route path="/about" element={<About/>} /> 
+        <Route path="/volunteer/*" element={<Board/>} />
+        <Route path="/board/*" element={<Login/>} />
+        <Route path="/mypage/*" element={<Manager/>} />
+        <Route path="/qna/*" element={<MyPage/>} />
+        <Route path="/login/*" element={<Qna/>} />
+        <Route path="/signup/*" element={<SignUp/>} />
+        <Route path="/manager/*" element={<Volunteer/>} />
       </Routes>
     </div>
   );
